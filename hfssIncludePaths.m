e45182a3c8f90b2a3ae5991f25b81be14ecd1589
danysan@gmail.com <danysan@gmail.com@316cfa60-a7ba-c5ed-481a-ebbdf42dfffa>
2013-05-30 19:26:50 +0000

    Add relative path as an optional argument.

--- hfssIncludePaths.m
+++ hfssIncludePaths.m
@@ -1,5 +1,5 @@
 % ----------------------------------------------------------------------------
-% function hfssIncludePaths()
+% function hfssIncludePaths([relPath = ''])
 % 
 % Description :
 % -------------
@@ -7,15 +7,21 @@
 %
 % Parameters :
 % ------------
-% None
+% relPath - Relative path from this function directory, in order to
+%           include the rest of the API directories. It must end with
+%			a slash ("/"). You must add first the API root directory
+%           manually to the Matlab path with the addpath command.
 %
 % Note :
 % ------
-% Call this function first before start using the API.
+% Call this function first before start using the API using the apropriate
+% relative path, in case your code is not in the root directory of the API
+% (see examples)
 %
 % Example :
 % ---------
 % hfssIncludePaths();
+% hfssIncludePaths('../../');
 % ----------------------------------------------------------------------------
 
 % ----------------------------------------------------------------------------
@@ -24,6 +30,7 @@
 % 30-Sept-2012: *Initial release.
 % 03-Octo-2012: *Add 'fieldsCalculator/' path.
 % 04-Nove-2012: *Add 'mesh/' path.
+% 30-May -2013: *Add relative path as an optional argument.
 % ----------------------------------------------------------------------------
 
 % ----------------------------------------------------------------------------
@@ -31,13 +38,17 @@
 % danysan@gmail.com / drprado@tsc.uniovi.es
 % 30 September 2012
 % ----------------------------------------------------------------------------
-function hfssIncludePaths()
+function hfssIncludePaths(relPath)
 
-addpath('boundary/');
-addpath('3dmodeler/');
-addpath('analysis/');
-addpath('general/');
-addpath('radiation/');
-addpath('reporter/');
-addpath('fieldsCalculator/');
-addpath('mesh/');
+if (nargin < 1)
+	relPath = '';
+end
+
+addpath([relPath, 'boundary/']);
+addpath([relPath, '3dmodeler/']);
+addpath([relPath, 'analysis/']);
+addpath([relPath, 'general/']);
+addpath([relPath, 'radiation/']);
+addpath([relPath, 'reporter/']);
+addpath([relPath, 'fieldsCalculator/']);
+addpath([relPath, 'mesh/']);

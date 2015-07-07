c246ab100f00053b503db5e697e4dc2615287123
rounaksingh <rounaksingh17@gmail.com>
2015-07-07 14:59:39 -0400

    hfssWavePort.m edited for Error(InHFSS): Geometry Required for assignment. Checked in HFSS Version-15

--- boundary/hfssAssignWavePort.m
+++ boundary/hfssAssignWavePort.m
@@ -71,14 +71,17 @@ fprintf(fid, '\n');
 fprintf(fid, 'oModule.AssignWavePort _\n');
 fprintf(fid, 'Array( _\n');
 fprintf(fid, '"NAME:%s", _\n', PortName);
+fprintf(fid, '"Faces:=", Array(%s), _\n', SheetObject);
 fprintf(fid, '"NumModes:=", %d, _\n', nModes); 
 fprintf(fid, '"PolarizeEField:=",  false, _\n');
 fprintf(fid, '"DoDeembed:=", false, _\n');
 fprintf(fid, '"DoRenorm:=", false, _\n');
 
+% Modes 
+fprintf(fid, 'Array("NAME:Modes", _\n');
+
 % Add the Mode-Lines One By One.
 for iM = 1:nModes,
-	fprintf(fid, 'Array("NAME:Modes", _\n');
 	if (isLine(iM))
 		fprintf(fid, 'Array("NAME:Mode1", _\n');
 		fprintf(fid, '"ModeNum:=",  %d, _\n', iM);
@@ -97,12 +100,14 @@ for iM = 1:nModes,
 	else
 		fprintf(fid, 'Array("NAME:Mode1", _\n');
 		fprintf(fid, '"ModeNum:=",  %d, _\n', iM);
-		fprintf(fid, '"UseIntLine:=", false) _\n');
+		fprintf(fid, '"UseIntLine:=", false)');
 	end;
 	if (iM ~= nModes)
 		fprintf(fid, ', _\n');
 	end;
 end;
-fprintf(fid, '), _\n');
-fprintf(fid, '"Objects:=", Array("%s")) \n', SheetObject);
+
+% End Modes Array and Waveport Array
+fprintf(fid, '))\n');
+end
                             

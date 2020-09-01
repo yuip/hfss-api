4eaf7108ffde41d751a5298e3f0a8a118b0436ee
yuip <danysan@gmail.com>
2020-09-01 11:20:41 +0200

    Revert changes on hfssAssignWavePort
    The previous changes gave error.

--- boundary/hfssAssignWavePort.m
+++ boundary/hfssAssignWavePort.m
@@ -71,18 +71,15 @@ fprintf(fid, '\n');
 fprintf(fid, 'oModule.AssignWavePort _\n');
 fprintf(fid, 'Array( _\n');
 fprintf(fid, '"NAME:%s", _\n', PortName);
-fprintf(fid, '"Faces:=", Array(%s), _\n', SheetObject);
 fprintf(fid, '"NumModes:=", %d, _\n', nModes); 
 fprintf(fid, '"PolarizeEField:=",  false, _\n');
 fprintf(fid, '"DoDeembed:=", false, _\n');
 fprintf(fid, '"DoRenorm:=", false, _\n');
 
-% Modes 
-fprintf(fid, 'Array("NAME:Modes", _\n');
-
 % Add the Mode-Lines One By One.
-for iM = 1:nModes,
-	if (isLine(iM))
+for iM = 1:nModes
+	fprintf(fid, 'Array("NAME:Modes", _\n');
+    if (isLine(iM))
 		fprintf(fid, 'Array("NAME:Mode1", _\n');
 		fprintf(fid, '"ModeNum:=",  %d, _\n', iM);
 		fprintf(fid, '"UseIntLine:=", true, _\n');
@@ -100,14 +97,11 @@ for iM = 1:nModes,
 	else
 		fprintf(fid, 'Array("NAME:Mode1", _\n');
 		fprintf(fid, '"ModeNum:=",  %d, _\n', iM);
-		fprintf(fid, '"UseIntLine:=", false)');
-	end;
-	if (iM ~= nModes)
+		fprintf(fid, '"UseIntLine:=", false) _\n');
+    end
+    if (iM ~= nModes)
 		fprintf(fid, ', _\n');
-	end;
-end;
-
-% End Modes Array and Waveport Array
-fprintf(fid, '))\n');
+    end
 end
-                            
+fprintf(fid, '), _\n');
+fprintf(fid, '"Objects:=", Array("%s")) \n', SheetObject);

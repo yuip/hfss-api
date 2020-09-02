87bfd5bb5e8158aab87d37cc094de9a4678de71d
yuip <danysan@gmail.com>
2020-09-02 10:46:53 +0200

    Revert "Revert "Add hfssSetVariable.""
    This reverts commit e69a9cfb7cbcdcb9a8f79e43b65c09530325a8f7.

---
+++ general/hfssSetVariable.m
@@ -0,0 +1,49 @@
+% ----------------------------------------------------------------------------
+% function function hfssSetVariable(fid, variable, value, units)
+% 
+% Description :
+% -------------
+% Create the VB Script necessary to create a new local variable in HFSS.
+%
+% Parameters :
+% ------------
+% fid      - file identifier of the HFSS script file.
+% variable - The name of the variable to be created.
+% value    - The value for the variable (DOUBLE).
+% units    - units of the variable (specify using either 'in', 'mm',
+%            'meter' or anything else defined in HFSS).
+%
+% Example :
+% ---------
+% fid = fopen('myantenna.vbs', 'wt');
+% ... 
+% hfssSetVariable(fid, 'radius', 5, 'cm')
+%
+% ----------------------------------------------------------------------------
+
+% ----------------------------------------------------------------------------
+% CHANGELOG
+%
+% 28-Sep-2015: *Initial release.
+% ----------------------------------------------------------------------------
+
+% ----------------------------------------------------------------------------
+% Written by Rounak Singh
+% rounaksingh17@gmail.com
+% 28 September 2015
+% ----------------------------------------------------------------------------
+function hfssSetVariable(fid, variable, value, units)
+	% Preamble.
+	fprintf(fid, '\n');
+
+	% Change the variable name
+	fprintf(fid, 'oDesign.ChangeProperty _\n');
+	fprintf(fid, 'Array("NAME:AllTabs", _\n');
+	fprintf(fid, 'Array("NAME:LocalVariableTab", _\n');
+	fprintf(fid, 'Array("NAME:PropServers",  _\n');
+	fprintf(fid, '"LocalVariables"), _\n');
+	fprintf(fid, 'Array("NAME:NewProps", _\n');
+	fprintf(fid, 'Array("NAME:%s", _\n', variable);
+    fprintf(fid, '"PropType:=", "VariableProp", _\n');
+    fprintf(fid, '"UserDef:=", true, _\n');
+    fprintf(fid, '"Value:=", "%f%s"))))\n', value, units);

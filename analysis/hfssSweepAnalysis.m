5d592c5756525ff6ef101019ead0c45dc6bb76bd
yuip <danysan@gmail.com>
2020-09-09 13:34:39 +0200

    Improvements in hfssSweepAnalysis
    *LINC sweep type supported.
    *Sync is now a cell array, allowing to syncronize variables in different groups.

--- analysis/hfssSweepAnalysis.m
+++ analysis/hfssSweepAnalysis.m
@@ -1,6 +1,6 @@
 % ----------------------------------------------------------------------------
-% function hfssSweepAnalysis(fid, Name, Analysis, Variables, Data, Units,
-%                            [Sync])
+% function hfssSweepAnalysis(fid, Name, Analysis, Type, Variables, Data,
+%                            Units, [Sync])
 % 
 % Description :
 % -------------
@@ -11,28 +11,36 @@
 % fid       - file identifier of the HFSS script file.
 % Name      - name of the sweep analysis to be created.
 % Analysis  - name of the associated analysis setup.
+% Type      - cell array with sweep types. Supported: 'LIN' and 'LINC'.
+%                "LIN Start Stop Step" -> Start:Step:Stop
+%                "LINC Start Stop Count" -> linspace(Start, Stop, Count)
 % Variables - cell array with the name of the variable of which the sweep
 %             will be performed.
 % Data      - cell array containing numerical values of [Start, Stop,
 %             Step] for each variable.
 % Units     - for Data, can be 'mm', 'cm', 'meters', etc.
-% Sync      - (optional) set to 1 to sync variable sweeps. Default: 0.
+% Sync      - (optional, cell array) set to 1, 2, ... to sync variable 
+%             sweeps in different groups. Default: 0.
 % 
 % Note :
 % ------
 %
 % Examples :
 % ---------
-% hfssSweepAnalysis(fid, 'ParSetup1', 'MySetup', 'var', [1, 9, 1], 'mm');
-% hfssSweepAnalysis(fid, 'ParSetup2', 'MySetup', {'pB', 'pA'}, ...
-%    {[0.1, 3.5, 0.2], [0.1, 3.5, 0.2]}, 'mm', 1);
+% hfssSweepAnalysis(fid, 'ParSetup1', 'MySetup', 'LIN', 'var', ...
+%    [1, 9, 1], 'mm');
+% hfssSweepAnalysis(fid, 'ParSetup2', 'MySetup', {'LIN', 'LIN'}, ...
+%     {'pB', 'pA'}, {[0.1, 3.5, 0.2], [0.1, 3.5, 0.2]}, 'mm', {1, 1});
 %
 % ----------------------------------------------------------------------------
 
 % ----------------------------------------------------------------------------
 % CHANGELOG
 %
-% 03-Sep-2020: *Initial release.
+% 03-Sep-2020: *Initial release (only LIN sweep type supported).
+% 09-Sep-2020: *LINC sweep type supported.
+%              *Sync is now a cell array, allowing to syncronize variables
+%               in different groups.
 % ----------------------------------------------------------------------------
 
 % ----------------------------------------------------------------------------
@@ -40,22 +48,28 @@
 % danysan@gmail.com / drprado@tsc.uniovi.es
 % 03 September 2020
 % ----------------------------------------------------------------------------
-function hfssSweepAnalysis(fid, Name, Analysis, Variables, Data, Units, ...
+function hfssSweepAnalysis(fid, Name, Analysis, Type, Variables, Data, Units, ...
                            Sync)
 
 % Arguments processor.
-if (nargin < 6)
+if (nargin < 7)
 	error('Insufficient # of arguments !');
 end
-if (nargin == 6)
-    Sync = 0;
+if (nargin == 7)
+    Sync = zeros(numel(Variables), 1);
 end
 
+if (~iscell(Type))
+    Type = {Type};
+end
 if (~iscell(Variables))
     Variables = {Variables};
 end
 if (~iscell(Data))
     Data = {Data};
+end
+if (~iscell(Sync))
+    Sync = num2cell(Sync);
 end
 
 % Preamble
@@ -77,10 +91,18 @@ fprintf(fid, '\tArray("NAME:Sweeps", _\n');
 for n = 1:numel(Variables)
     fprintf(fid, '\t\tArray("NAME:SweepDefinition", _\n');
     fprintf(fid, '\t\t"Variable:=", "%s", _\n', Variables{n});
-    fprintf(fid, '\t\t"Data:=", "LIN %f%s %f%s %f%s", _\n', ...
-        Data{n}(1), Units, Data{n}(2), Units, Data{n}(3), Units);
+    switch Type{n}
+        case 'LIN'
+            fprintf(fid, '\t\t"Data:=", "LIN %f%s %f%s %f%s", _\n', ...
+                Data{n}(1), Units, Data{n}(2), Units, Data{n}(3), Units);
+        case 'LINC'
+            fprintf(fid, '\t\t"Data:=", "LINC %f%s %f%s %i", _\n', ...
+                Data{n}(1), Units, Data{n}(2), Units, Data{n}(3));
+        otherwise
+            error('Wrong type!');
+    end
     fprintf(fid, '\t\t"OffsetF1:=", false, _\n');
-    fprintf(fid, '\t\t"Synchronize:=", %i)', Sync);
+    fprintf(fid, '\t\t"Synchronize:=", %i)', Sync{n});
     
     if (n < numel(Variables))
         fprintf(fid, ', _\n'); % Don't include comma in last item.
